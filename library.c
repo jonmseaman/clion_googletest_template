dbb877d46c51bf28703062e936c422b52854a211
Jon Seaman <jonmseaman@live.com>
2018-01-11 20:33:57 -0500

    Created basic template project

---
+++ library.c
@@ -0,0 +1,10 @@
+//
+// Created by jonms on 1/11/2018.
+//
+
+#include "library.h"
+
+int func1()
+{
+    return 1;
+}
---
+++ library.h
@@ -0,0 +1,10 @@
+//
+// Created by jonms on 1/11/2018.
+//
+
+#ifndef COUNT_LIBRARY_H
+#define COUNT_LIBRARY_H
+
+int func1();
+
+#endif //COUNT_LIBRARY_H
